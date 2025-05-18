cf6277bd17ddd51197a0a8bbf98260817138e980
gadisom <jeongwon.kim@gmail.com>
2025-05-18 16:32:47 +0900

    first commit

---
+++ MVP_MacOS/MVP_MacOS/MVP_MacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVP_MacOSApp.swift
+//  MVP_MacOS
+//
+//  Created by 김정원 on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MVP_MacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
