06597426313e859d4b7dd7e935c782824cc7c16a
gadisom <jeongwon.kim@gmail.com>
2025-05-19 14:20:07 +0900

    chore: 클린 아키텍쳐 구조 적용

---
+++ MVP_MacOS/MVP_MacOS/Presentation/Analysis/AnalysisView.swift
@@ -0,0 +1,8 @@
+//
+//  AnalysisView.swift
+//  MVP_MacOS
+//
+//  Created by 김정원 on 5/18/25.
+//
+
+import Foundation
---
+++ MVP_MacOS/MVP_MacOS/Presentation/Analysis/AnalysisViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AnalysisViewModel.swift
+//  MVP_MacOS
+//
+//  Created by 김정원 on 5/18/25.
+//
+
+import Foundation
