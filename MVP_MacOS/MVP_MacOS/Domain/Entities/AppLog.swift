3b4f2353f9687dc06874b5266cd2ff9634131488
gadisom <jeongwon.kim@gmail.com>
2025-05-20 15:31:43 +0900

    Feat: SwiftDataManager DI

--- MVP_MacOS/MVP_MacOS/Domain/Entities/AppLog.swift
+++ MVP_MacOS/MVP_MacOS/Domain/Entities/AppLog.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+struct AppLog {
+    var id: UUID
+    var timestamp: Date
+    var duration: TimeInterval
+    var title: String
+    var app: String
+    var url: String?
+}
