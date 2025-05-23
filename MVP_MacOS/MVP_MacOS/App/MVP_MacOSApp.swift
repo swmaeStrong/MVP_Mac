a1d30039ae917bebdf259f54927a317f66a07c57
gadisom <jeongwon.kim@gmail.com>
2025-05-23 18:56:37 +0900

    fix: enusureAccessibility , userID

--- MVP_MacOS/MVP_MacOS/App/MVP_MacOSApp.swift
+++ MVP_MacOS/MVP_MacOS/App/MVP_MacOSApp.swift
@@ -20,3 +20,14 @@ struct MVP_MacOSApp: App {
         .modelContainer(for: [AppLogEntity.self])
     }
 }
+
+func ensureAccessibilityPermission() {
+    let options = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true] as CFDictionary
+    let accessEnabled = AXIsProcessTrustedWithOptions(options)
+
+    if !accessEnabled {
+        print("⚠️ 손쉬운 사용 권한이 필요합니다. 설정에서 앱을 허용해주세요.")
+    } else {
+        print("✅ 손쉬운 사용 권한 확인됨")
+    }
+}
