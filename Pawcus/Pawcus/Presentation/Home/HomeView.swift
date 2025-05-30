ef0aed4faa34f09bdb328072dc843540290c1a1c
gadisom <jeongwon.kim@gmail.com>
2025-05-30 15:45:45 +0900

    fix: 최소 크기 지정

--- Pawcus/Pawcus/Presentation/Home/HomeView.swift
+++ Pawcus/Pawcus/Presentation/Home/HomeView.swift
@@ -16,78 +16,53 @@ struct HomeView: View {
     @Injected(\.activityLogger) private var appUsageLogger: ActivityLogger
     
     var body: some View {
-        HStack(alignment: .top, spacing: 32) {
-            VStack(spacing: 20) {
-                Text("\(timeStore.seconds.formattedHMSFromSeconds)")
-                    .monospaced()
-                    .transition(.opacity)
-                    .animation(.easeInOut(duration: 0.7), value: timeStore.seconds.formattedHMSFromSeconds)
-                    .font(.system(size: 70, weight: .bold, design: .monospaced))
-                    .foregroundStyle(.white)
-
-                HStack(spacing: 10){
-                    
-                    Button(action: {
-                        if timeStore.isRunning {
-                            timeStore.stop()
-                            appUsageLogger.stopLogging()
-                        } else {
-                            timeStore.start()
-                            appUsageLogger.configure(context: modelContext)
-                        }
-                    }) {
-                        Image(systemName: timeStore.isRunning ? "stop.fill" : "play.fill")
-                            .font(.title3)
-                            .padding()
-                            .background(
-                                Capsule()
-                                    .fill(timeStore.isRunning ? Color.indigo.opacity(0.9): Color.red.opacity(0.4))
-                            )
-                            .foregroundColor(.white)
-                    }
-                    .buttonStyle(.plain)
-                    Button(action: {
-                        
-                    }) {
-                        Image(systemName: "arrow.trianglehead.counterclockwise")
-                            .font(.title2)
-                            .foregroundColor(.white)
-                    }
-                    .buttonStyle(.plain)
+        VStack(spacing: 20) {
+            Text("Timer")
+                .monospaced()
+                .bold()
+                .font(.largeTitle)
+            Text("\(timeStore.seconds.formattedHMSFromSeconds)")
+                .monospaced()
+                .transition(.opacity)
+                .animation(.easeInOut(duration: 0.7), value: timeStore.seconds.formattedHMSFromSeconds)
+                .font(.system(size: 70, weight: .bold, design: .monospaced))
+            
+            HStack(spacing: 10){
                 
+                Button(action: {
+                    if timeStore.isRunning {
+                        timeStore.stop()
+                        appUsageLogger.stopLogging()
+                    } else {
+                        timeStore.start()
+                        appUsageLogger.configure(context: modelContext)
+                    }
+                }) {
+                    Image(systemName: timeStore.isRunning ? "stop.fill" : "play.fill")
+                        .font(.title3)
+                        .padding()
+                        .background(
+                            Capsule()
+                                .fill(timeStore.isRunning ? Color.indigo.opacity(0.9): Color.red.opacity(0.6))
+                        )
+                        .foregroundColor(.secondary)
                 }
+                .buttonStyle(.plain)
+                Button(action: {
+                    
+                }) {
+                    Image(systemName: "arrow.trianglehead.counterclockwise")
+                        .font(.title2)
+                }
+                .buttonStyle(.plain)
+                
             }
+            .foregroundStyle(.secondary)
         }
         .navigationTitle("Home")
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .frame(minWidth: 400, minHeight: 100)
+        .frame(minWidth: 400, minHeight: 200)
         .foregroundColor(.primary)
-        .background(
-            LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.9), Color.indigo.opacity(0.7)]),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
-            .ignoresSafeArea()
-        )
-        .toolbar {
-            ToolbarItem(placement: .automatic) {
-                Button {
-                    Task {
-                        await timeStore.sendLogs()
-                    }
-                } label: {
-                    Label("Send Logs", systemImage: "paperplane.fill")
-                }
-            }
-            ToolbarItem(placement: .automatic) {
-                Button {
-                    Task {
-                        await timeStore.deleteLogs()
-                    }
-                } label: {
-                    Label("Delete Logs", systemImage: "trash")
-                }
-            }
-        }
     }
 }
 
@@ -95,25 +70,5 @@ struct HomeView: View {
     HomeView()
         .modelContainer(for: [AppLogEntity.self], inMemory: true)
         .environmentObject(DailyWorkTimeStore())
-
-}
-
-struct TimerCard: View {
-    let value: Int
-    //let label: String
-
-    var body: some View {
-        VStack(spacing: 4) {
-            ZStack {
-                Text(String(value))
-                    .font(.system(size: 40, weight: .bold, design: .monospaced))
-                    .foregroundColor(.white)
-                    .id(value)
-                    .transition(.opacity)
-                    .animation(.easeInOut(duration: 0.8), value: value)
-            }
-        }
-        .padding()
-        .frame(width: 100, height: 100)
-    }
+    
 }
