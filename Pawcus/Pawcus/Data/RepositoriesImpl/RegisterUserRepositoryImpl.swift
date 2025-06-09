d02f352eb0e2a53cd9d68775006ee0e7f6cca911
gadisom <jeongwon.kim@gmail.com>
2025-06-09 16:26:28 +0900

    feat: 소셜 로그인 accessToken 전송 후 정식 회원 등록 로직 구현

--- Pawcus/Pawcus/Data/RepositoriesImpl/RegisterUserRepositoryImpl.swift
+++ Pawcus/Pawcus/Data/RepositoriesImpl/RegisterUserRepositoryImpl.swift
@@ -30,6 +30,17 @@ final class RegisterUserRepositoryImpl: RegisterUserRepository {
         return true
     }
     
+    func registerSocialUser(accessToken: String) async throws {
+        let tokenData = try await service.registerSocialUser(accessToken: accessToken)
+        // 네트워크 호출 결과로 받은 토큰을 로컬에 저장
+        KeychainHelper.standard.save(tokenData.accessToken,
+                                     service: "com.pawcus.token",
+                                     account: "accessToken")
+        KeychainHelper.standard.save(tokenData.refreshToken,
+                                     service: "com.pawcus.token",
+                                     account: "refreshToken")
+    }
+    
     func getGuestToken() async throws {
         let tokenData = try await service.getGuestToken()
         print("토큰: \(tokenData.accessToken)")
--- Pawcus/Pawcus/Presentation/Login/LoginViewModel.swift
+++ Pawcus/Pawcus/Presentation/Login/LoginViewModel.swift
@@ -10,16 +10,25 @@ import Foundation
 import Combine
 import Supabase
 import SwiftUI
+import Factory
 
 @MainActor
 final class LoginViewModel: ObservableObject {
     @AppStorage("userNickname") private var nickname: String = ""
     @Published var showUsernamePrompt = false
+    @Injected(\.registerUserUseCase) private var registerUserUseCase
     private let supabaseAuthService: SupabaseAuthService = SupabaseAuthService()
     
     func loginWithGoogle() async {
         let success = await supabaseAuthService.loginWithGoogle()
         if success {
+            do {
+                 let accessToken = try await supabase.auth.session.accessToken.description
+                try await registerUserUseCase.registerSocialUser(accessToken: accessToken)
+            } catch {
+                
+            }
+           
             showUsernamePrompt = true
         }
     }
